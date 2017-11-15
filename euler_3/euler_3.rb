1fa6673226eafa9dd158bf462a873bbb3e20ca53
Matt Behan <mattbehan@gmail.com>
2017-11-15 06:38:24 -0500

    commit 3

---
+++ euler_3/euler_3.rb
@@ -0,0 +1,20 @@
+num = 600851475143
+
+def return_prime_factors number
+
+
+
+end
+
+def prime_numbers_up_to
+
+
+
+end
+
+# method one:
+# do the mathematical way for prime factorization... take each number and try to divide it by the smallest number. potentially log(n) squared
+
+# method two:
+# find all the prime factors up to that number. try to divide it by each one of it until the last number divides evenly
+# sqrt(n) for 
