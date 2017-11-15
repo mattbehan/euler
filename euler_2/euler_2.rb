c7617fb376e4917d92ffb22ea65b4c825137c3a9
Matt Behan <mattbehan@gmail.com>
2017-11-15 18:44:08 -0500

    solved number 2

--- euler_2/euler_2.rb
+++ euler_2/euler_2.rb
@@ -1,5 +1,25 @@
-returns the first n fibonacci numbers, we want this to take in a certain number and return all values 
+# returns the first n fibonacci numbers, we want this to take in a certain number and return all values 
 
-def fibonacci (n)
-	return fibonacci(n-1) + fibonacci(n-2)
+# def fibonacci (n)
+# 	return fibonacci(n-1) + fibonacci(n-2)
+# end
+
+
+first = 1
+second = 2
+next_num = 3
+
+series = [first, second]
+
+until next_num > 4000000
+
+	series << next_num
+
+	placeholder = next_num + second
+	first = second
+	second = next_num
+	next_num = placeholder
+
 end
+
+print series.select{|num| num.even? }.sum
